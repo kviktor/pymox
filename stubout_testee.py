3d0131a4f9b6608590668fa8b80bf95cac93e8a9
Ivan Neto <ivan.cr.neto@gmail.com>
2018-04-21 21:46:43 -0300

    flake8 fix

--- stubout_testee.py
+++ stubout_testee.py
@@ -12,5 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 def SampleFunction():
     raise Exception('I should never be called!')
